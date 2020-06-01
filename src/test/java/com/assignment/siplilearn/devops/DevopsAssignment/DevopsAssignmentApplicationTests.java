5a0095cfac401f24597872d0eb8dd87deec3e51c
Ubuntu <vineetkumarmind@vineetkumarmind.3czovn11eexejm5abjggqeaxrb.gx.internal.cloudapp.net>
2020-06-01 20:05:22 +0000

    commiting folders

---
+++ src/test/java/com/assignment/siplilearn/devops/DevopsAssignment/DevopsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.siplilearn.devops.DevopsAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
